b383f1b116ef71e0653172fcfb7e5563cf8352e8
Cassio Koshikumo <ckoshikumo@gmail.com>
2018-11-15 10:03:52 -0200

    Some reordering and change output format

--- audit.h
+++ audit.h
@@ -5,8 +5,8 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define AUDIT_MAX_MESSAGES_ 50
-#define AUDIT_MAX_MESSAGE_LENGTH_ 256
+#define AUDIT_MAX_MESSAGES 50
+#define AUDIT_MAX_MESSAGE_LENGTH 256
 
 #ifdef AUDIT_NO_COLORS
 #define RED_
@@ -20,95 +20,95 @@
 #define RESET_   "\x1b[0m"
 #endif
 
-int audit_total_tests_ = 0;
-int audit_total_asserts_ = 0;
-int audit_local_failed_asserts_ = 0;
-int audit_failed_asserts_ = 0;
-int audit_failed_tests_ = 0;
-int audit_next_message_ = 0;
-int audit_current_dot_ = 0;
+typedef char audit_message[AUDIT_MAX_MESSAGE_LENGTH];
+audit_message audit_messages[AUDIT_MAX_MESSAGES];
 
-typedef char audit_message_[AUDIT_MAX_MESSAGE_LENGTH_];
-audit_message_ audit_messages_[AUDIT_MAX_MESSAGES_];
+int audit_total_tests = 0;
+int audit_total_asserts = 0;
+int audit_local_failed_asserts = 0;
+int audit_failed_asserts = 0;
+int audit_failed_tests = 0;
+int audit_next_message = 0;
+int audit_current_dot = 0;
+
+static inline void _audit_print_failures(void)
+{
+	printf("\n\n");
+	if (audit_next_message == 0 ) return;
+	for (int i = 0; i < audit_next_message; ++i) {
+		printf("%s\n", audit_messages[i]);
+	}
+	printf("\n");
+}
+
+static inline void _audit_check_message_count(void)
+{
+	if (audit_next_message < AUDIT_MAX_MESSAGES) return;
+
+	_audit_print_failures();
+	printf(RED_ "TOO MANY ERRORS! Aborting." RESET_ "\n");
+	exit(EXIT_FAILURE);
+}
+
+static inline void _audit_summary(void)
+{
+	printf("\n\n");
+
+	if (audit_failed_tests == 0) {
+		printf(GREEN_ "AUDITING OK" RESET_);
+	} else {
+		printf(RED_ "AUDITING FAILED" RESET_);
+	}
+
+	_audit_print_failures();
+
+	printf("%i tests (%i failed), %i assertions (%i failed)\n",
+	       audit_total_tests, audit_failed_tests,
+	       audit_total_asserts, audit_failed_asserts);
+}
+
+#define CHECK(name_) void name_(void)
+
+static inline void audit_register(void (*testFn)(void))
+{
+	audit_total_tests++;
+	audit_local_failed_asserts = 0;
+	testFn();
+}
 
 #define AUDIT(assert_, msg_, ...) do {\
-		audit_total_asserts_++;\
-		audit_current_dot_++;\
-		if (audit_current_dot_ == 80) {\
+		audit_total_asserts++;\
+		audit_current_dot++;\
+		if (audit_current_dot == 80) {\
 			printf("\n");\
-			audit_current_dot_ = 0;\
+			audit_current_dot = 0;\
 		}\
 		if (assert_) {\
 			printf(GREEN_ "." RESET_);\
 			break;\
 		}\
 		printf(RED_ "F" RESET_);\
-		audit_failed_asserts_++;\
-		if (audit_local_failed_asserts_ == 0) {\
-			audit_failed_tests_++;\
-			audit_check_message_count_();\
-			snprintf(audit_messages_[audit_next_message_], AUDIT_MAX_MESSAGE_LENGTH_,\
+		audit_failed_asserts++;\
+		if (audit_local_failed_asserts == 0) {\
+			audit_failed_tests++;\
+			_audit_check_message_count();\
+			snprintf(audit_messages[audit_next_message], AUDIT_MAX_MESSAGE_LENGTH,\
 			         YELLOW_ "%s" RESET_, __func__);\
-			audit_next_message_++;\
+			audit_next_message++;\
 		}\
-		audit_check_message_count_();\
-		snprintf(audit_messages_[audit_next_message_], AUDIT_MAX_MESSAGE_LENGTH_,\
-		         "\t%i: " msg_, __LINE__, ##__VA_ARGS__);\
-		audit_next_message_++;\
+		_audit_check_message_count();\
+		snprintf(audit_messages[audit_next_message], AUDIT_MAX_MESSAGE_LENGTH,\
+		         "\t[%i] " msg_, __LINE__, ##__VA_ARGS__);\
+		audit_next_message++;\
 	} while (0)
-
-static inline void audit_run(void (*testFn)(void))
-{
-	audit_total_tests_++;
-	audit_local_failed_asserts_ = 0;
-	testFn();
-}
-
-static inline void audit_print_failures_(void)
-{
-	printf("\n\n");
-	if (audit_next_message_ == 0 ) return;
-	for (int i = 0; i < audit_next_message_; ++i) {
-		printf("%s\n", audit_messages_[i]);
-	}
-	printf("\n");
-}
-
-static inline void audit_check_message_count_(void)
-{
-	if (audit_next_message_ < AUDIT_MAX_MESSAGES_) return;
-
-	audit_print_failures_();
-	printf(RED_ "TOO MANY ERRORS! Aborting." RESET_ "\n");
-	exit(EXIT_FAILURE);
-}
-
-static inline void audit_summary_(void)
-{
-	printf("\n\n");
-
-	if (audit_failed_tests_ == 0) {
-		printf(GREEN_ "ALL TESTS PASSED" RESET_);
-	} else {
-		printf(RED_ "TEST SUITE FAILED" RESET_);
-	}
-
-	audit_print_failures_();
-
-	printf("%i tests (%i failed), %i assertions (%i failed)\n",
-	       audit_total_tests_, audit_failed_tests_,
-	       audit_total_asserts_, audit_failed_asserts_);
-}
-
-#define CHECK(name_) void name_(void)
 
 #define AUDIT_RUN(test_suite_)\
 	int main(void) {\
-		printf(GREEN_ "BEGIN AUDITING" RESET_ "\n");\
-		printf("Test suite: %s\n\n", __FILE__);\
+		printf(GREEN_ "START AUDITING" RESET_ "\n");\
+		printf("%s\n", __FILE__);\
 		test_suite_();\
-		audit_summary_();\
-		return audit_failed_tests_ == 0 ? 0 : -1;\
+		_audit_summary();\
+		return audit_failed_tests == 0 ? 0 : -1;\
 	}
 
 #endif // _AUDIT_H_
